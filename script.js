13a5264bf29edc28708dbab9377b16ff5ea0205a
Triffana A <triffanaantony@gmail.com>
2026-06-03 22:33:11 +0530

    Updated portfolio

--- script.js
+++ script.js
@@ -4,24 +4,13 @@ document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
-        const target = document.querySelector(this.getAttribute('href'));
+        const target = document.querySelector(
+            this.getAttribute('href')
+        );
 
         window.scrollTo({
             top: target.offsetTop - 100,
             behavior: 'smooth'
         });
     });
-});
-
-fetch("https://api.countapi.xyz/hit/triffana-portfolio/visits")
-.then(res => res.json())
-.then(data => {
-    const counter = document.getElementById("visitor-count");
-
-    if(counter){
-        counter.innerText = data.value;
-    }
-})
-.catch(error => {
-    console.error("Counter Error:", error);
 });
