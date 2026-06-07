0bdf07368c5f5b654fb1977f3eff83597f6145dc
Triffana A <triffanaantony@gmail.com>
2026-06-07 14:02:12 +0530

    Added mobile navbar

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
+const menuToggle =
+document.getElementById("menu-toggle");
+
+const navLinks =
+document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+document.querySelectorAll("#nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
